58fd9159d9d97b4bbfddc54d1d6c6452f3a0a6e2
muszi1 <113166638+muszi1@users.noreply.github.com>
2022-10-08 17:55:51 +0200

    Add files via upload

--- házi4.py
+++ házi4.py
@@ -1,5 +1,24 @@
-#4.feladat
-bekertszam=int(input("Adjon meg egy szamot "))
-print(bin(bekertszam)+" lesz a bináris alakja.")
+def valid(text,chars):
+    for x in text:
+        for y in chars:
+            if x==y:
+                print(x)
+text=input("Írj be valalmit:")
+chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+valid(text,chars)
 
-def main():
+def szavak(szó1,szó2):
+    print(szó2+" "+szó1)
+szó1=input("Adjon meg az első szót ")
+szó2=input("Adja meg a második szót ")
+szavak(szó1,szó2)
+
+
+code="kwwsv=22|rxwx1eh2gTz7z<Zj[fT"
+asd=[]
+megfejtes=[]
+for x  in code:
+    asd.append(ord(x)-3)
+for i in asd:
+    megfejtes.append(chr(i))
+print("\n".join(megfejtes),end="")
